403314e425f3d2c4354efd43dd826af66708143c
evcodez <90653220+eviekwak@users.noreply.github.com>
2023-06-13 23:57:54 -0700

    added inventory UML

--- src/main/java/com/example/project/Inventory.java
+++ src/main/java/com/example/project/Inventory.java
@@ -1,7 +1,45 @@
+package com.example.project;
+import com.example.project.Part;
+import com.example.project.Product;
+import javafx.collections.ObservableList;
+
 public class Inventory {
-    
 
+    private ObservableList<Part> allParts;
+    private ObservableList<Product> allProducts;
 
+    public void addPart(Part newPart) {
+
+    }
+    public void addProduct(Product newProduct) {
+
+    }
+    public Part lookupPart(int partId){
+
+        return null;
+    }
+    public Product lookupProduct(int partId){
+
+        return null;
+    }
+    public void updatePart(int index, Part selectedPart){
+
+    }
+    public void updateProduct(int index, Product newProduct){
+
+    }
+    public boolean deletePart(Part selectedPart){
+        return true;
+    }
+    public boolean deleteProduct(Product selectedProduct){
+
+    }
+    public ObservableList<Part> getAllParts(){
+
+    }
+    public ObservableList<Product> getAllProducts(){
+
+    }
 }
 
 
