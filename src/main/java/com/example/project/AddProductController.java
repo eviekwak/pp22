cfa006fb0f1accdc818325f8ffd5b85453aceada
remsuf <68096514+remsuf@users.noreply.github.com>
2023-07-28 20:08:32 -0700

    errors

--- src/main/java/com/example/project/AddProductController.java
+++ src/main/java/com/example/project/AddProductController.java
@@ -1,12 +1,15 @@
 package com.example.project;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
@@ -19,11 +22,27 @@ public class AddProductController implements Initializable {
     private Stage stage;
     private Scene scene;
 
+    @FXML
+    public TextField IDField, NameField,InvField, PriceField, MinField,MaxField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Initialized addanyproduct");
         int predeterminedID = Model.getInstance().returnProductID();
-        IHIDField.setText(String.valueOf(predeterminedID));
+        IDField.setText(String.valueOf(predeterminedID));
+
+        System.out.println("Initialized home");
+        PartIDColumnHome.setCellValueFactory(new PropertyValueFactory<Part, Integer>("id"));
+        PartNameColumnHome.setCellValueFactory(new PropertyValueFactory<Part, String>("name"));
+        PartInventLvlColumnHome.setCellValueFactory(new PropertyValueFactory<Part, Integer>("stock"));
+        PartPriceColumnHome.setCellValueFactory(new PropertyValueFactory<>("price"));
+        PartsTableviewHome.setItems(Model.getInstance().getInventory().getAllParts());
+
+        ProductIDColumnHome.setCellValueFactory(new PropertyValueFactory<Product, Integer>("id"));
+        ProductNameColumnHome.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
+        ProductInventLvlColumnHome.setCellValueFactory(new PropertyValueFactory<Product, Integer>("stock"));
+        ProductPriceColumnHome.setCellValueFactory(new PropertyValueFactory<>("price"));
+        ProductsTableviewHome.setItems(Model.getInstance().getInventory().getAllProducts());
     }
 
     public void SaveButton(ActionEvent actionEvent) {
